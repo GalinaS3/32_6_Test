de395d355e151e604dc6876947d847886a29cb6c
GalinaS3 <116189685+GalinaS3@users.noreply.github.com>
2023-06-05 11:59:17 +0500

    Add files via upload

---
+++ ThatUsesDB.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include"DBConnection.h"
+//include <mysql.h> //для Windows
+#include<mysql/mysql.h>
+
+
+class ThatUsesDB : public IDBConnection
+{
+private:
+    MYSQL mysql;
+    MYSQL_RES* res;
+    MYSQL_ROW row;
+    
+public:
+    ThatUsesDB();
+    ~ThatUsesDB();
+
+    void openDB(const char* root, const char* pass, const char* nameDB) override;
+    
+    void closeDB()override;
+
+    void createTable() override;
+
+    bool insertUser(const std::string& name, const std::string& login, const std::string& password) override;
+    
+    void printUsers() override;
+
+    void deleteTable()override;
+};
+
---
+++ main.cpp
@@ -0,0 +1,21 @@
+﻿#include "MyTestSuite.h"
+
+
+int main(int argc, char* argv[])
+{
+    setlocale(LC_ALL, "");
+    
+    //ThatUsesDB dbconnect;
+    //dbconnect.openDB("***", "***", "***"); //вместо звёздочек надо ввести юзера, пароль и имя БД
+    //dbconnect.createTable();
+    //dbconnect.insertUser("Masha", "Mamasha", "12345");
+    //dbconnect.insertUser("Ivan", "Vano", "54321");
+    //dbconnect.printUsers();
+    ////dbconnect.deleteTable();
+    //dbconnect.closeDB();
+
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+
+    return  0;
+}
